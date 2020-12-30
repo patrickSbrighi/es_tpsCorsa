4de951b64e46f613f84911fe2c8d7e8474160dd5
patrickSbrighi <71379720+patrickSbrighi@users.noreply.github.com>
2020-12-30 21:56:19 +0100

    Creazione movimento e sistemazione delle immagini

--- es_corsa/MainWindow.xaml.cs
+++ es_corsa/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace es_corsa
 {
@@ -23,6 +24,65 @@ namespace es_corsa
         public MainWindow()
         {
             InitializeComponent();
+            Thread t1 = new Thread(new ThreadStart(muoviSqualo1));
+            Thread t2 = new Thread(new ThreadStart(muoviSqualo2));
+            Thread t3 = new Thread(new ThreadStart(muoviSqualo3));
+            t1.Start();
+            t2.Start(); t3.Start();
+        }
+
+       int posizionePartenza1 = 83;
+        int posizionePartenza2 = 83;
+        int posizionePartenza3 = 83;
+        public void muoviSqualo1()
+        {
+            while (posizionePartenza1 < 500)
+            {
+                posizionePartenza1 += 5;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgShark1.Margin = new Thickness(posizionePartenza1, 10, 0, 0);
+                }));
+            }
+
+
+        }
+
+        public void muoviSqualo2()
+        {
+            while (posizionePartenza2 < 500)
+            {
+                posizionePartenza2 += 5;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgShark2.Margin = new Thickness(posizionePartenza2, 146, 0, 0);
+                }));
+            }
+
+
+        }
+
+        public void muoviSqualo3()
+        {
+            while (posizionePartenza3 < 500)
+            {
+                posizionePartenza3 += 5;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgShark3.Margin = new Thickness(posizionePartenza3, 287, 0, 0);
+                }));
+            }
+
+
         }
     }
 }
